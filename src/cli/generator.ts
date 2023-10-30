c1ae3235ee299df28526658e326ae73c82d1d88b
Andrew LaMarche <andrew.lamarche@uconn.edu>
2023-10-29 22:25:40 -0400

    Made simple SphinxNeeds CLI command

--- src/cli/generator.ts
+++ src/cli/generator.ts
@@ -1,20 +1,55 @@
-// import fs from 'fs';
-// import { CompositeGeneratorNode, NL, toString } from 'langium';
-// import path from 'path';
-// import { Model } from '../language/generated/ast';
-// import { extractDestinationAndName } from './cli-util';
+import fs from 'fs';
+import { CompositeGeneratorNode, Reference, toString } from 'langium';
+import path from 'path';
+import { Goal, Model, Requirement } from '../language/generated/ast';
+import { extractDestinationAndName } from './cli-util';
 
-// export function generateReqIF(model: Model, filePath: string, destination: string | undefined): string {
-//     const data = extractDestinationAndName(filePath, destination);
-//     const generatedFilePath = `${path.join(data.destination, data.name)}.js`;
+export type NeedsObject = "Goal" | "Requirement" 
 
-//     const fileNode = new CompositeGeneratorNode();
-//     fileNode.append('"use strict";', NL, NL);
-//     model.greetings.forEach(greeting => fileNode.append(`console.log('Hello, ${greeting.person.ref?.name}!');`, NL));
+export function generateSphinxNeedsObjects(model: Model, type: NeedsObject = "Requirement", filePath: string, destination: string | undefined): string {
+    const data = extractDestinationAndName(filePath, destination);
+    const generatedFilePath = `${path.join(data.destination, data.name)}.rst`;
 
-//     if (!fs.existsSync(data.destination)) {
-//         fs.mkdirSync(data.destination, { recursive: true });
-//     }
-//     fs.writeFileSync(generatedFilePath, toString(fileNode));
-//     return generatedFilePath;
-// }
+    const fileNode = new CompositeGeneratorNode();
+    let arrayRefStringer = (array: Array<any>, tag:string) => {
+        if (array === undefined || array === null) return
+        if (!Object.hasOwn(array, 'length')) return
+        return `${array.length ? `\t:${tag}: ` + (array as Reference[]).map(val => val.$nodeDescription!!.name).join(', ') + '\n' : ''}`
+    }
+
+    model.items.forEach(item => {
+        if (item.$type === type) {
+            switch (item.$type) { // if a given model item is the type we are seeking
+                case "Goal": 
+                    let goal = item as Goal
+                    fileNode.append(`.. need:: ${goal.title ?? 'Placeholder Title'}\n` +
+                                        `\t:id: ${goal.name}\n` +
+                                        `${arrayRefStringer(goal.categories, 'tags')}` +
+                                        `${arrayRefStringer(goal.referencedGoal, 'referencedGoals')}` +
+                                        `${arrayRefStringer(goal.evolvesGoals, 'evolves')}` +
+                                        `${arrayRefStringer(goal.refinesGoals, 'refines')}` +
+                                        `${arrayRefStringer(goal.conflictingGoals, 'conflictsWith')}\n`
+                    )
+                    break
+                case "Requirement":
+                    let req = item as Requirement
+                    fileNode.append(`.. req:: ${req.title ?? 'Placeholder Title'}\n` +
+                                        `\t:id: ${req.name}\n` +
+                                        `${arrayRefStringer(req.categories, 'tags')}` +
+                                        `${arrayRefStringer(req.referencedGoal, 'referencedGoals')}` +
+                                        `${arrayRefStringer(req.evolvesReqs, 'evolves')}` +
+                                        `${arrayRefStringer(req.refinesReqs, 'refines')}` +
+                                        `${arrayRefStringer(req.inheritsReqs, 'inherits')}` +
+                                        `${arrayRefStringer(req.mitigatedHazards, 'mitigates')}` +
+                                        `${arrayRefStringer(req.decomposesReqs, 'decomposes')}\n`)
+                    break
+            }
+        }
+    })
+
+    if (!fs.existsSync(data.destination)) {
+        fs.mkdirSync(data.destination, { recursive: true });
+    }
+    fs.writeFileSync(generatedFilePath, toString(fileNode));
+    return generatedFilePath;
+}
--- src/cli/main.ts
+++ src/cli/main.ts
@@ -1,37 +1,39 @@
-// import chalk from 'chalk';
-// import { Command } from 'commander';
-// import { Model } from '../language/generated/ast';
-// import { ReqSpecLanguageMetaData } from '../language/generated/module';
-// import { createReqSpecServices } from '../language/req-spec-module';
-// import { extractAstNode } from './cli-util';
-// // import { generateJavaScript } from './generator';
-// import { NodeFileSystem } from 'langium/node';
+import chalk from 'chalk';
+import { Command } from 'commander';
+import { Model } from '../language/generated/ast';
+import { ReqSpecLanguageMetaData } from '../language/generated/module';
+import { createReqSpecServices } from '../language/req-spec-module';
+import { extractAstNode } from './cli-util';
+// import { generateJavaScript } from './generator';
+import { NodeFileSystem } from 'langium/node';
+import { generateSphinxNeedsObjects, NeedsObject } from './generator';
 
-// export const generateAction = async (fileName: string, opts: GenerateOptions): Promise<void> => {
-//     const services = createReqSpecServices(NodeFileSystem).ReqSpec;
-//     const model = await extractAstNode<Model>(fileName, services);
-//     const generatedFilePath = generateJavaScript(model, fileName, opts.destination);
-//     console.log(chalk.green(`JavaScript code generated successfully: ${generatedFilePath}`));
-// };
+export const generateAction = async (fileName: string, type: NeedsObject, opts: GenerateOptions): Promise<void> => {
+    const services = createReqSpecServices(NodeFileSystem).ReqSpec;
+    const model = await extractAstNode<Model>(fileName, services);
+    const generatedFilePath = generateSphinxNeedsObjects(model, type, fileName, opts.destination);
+    console.log(chalk.green(`Sphinx Needs code generated successfully: ${generatedFilePath}`));
+};
 
-// export type GenerateOptions = {
-//     destination?: string;
-// }
+export type GenerateOptions = {
+    destination?: string;
+}
 
-// export default function(): void {
-//     const program = new Command();
+export default function(): void {
+    const program = new Command();
 
-//     program
-//         // eslint-disable-next-line @typescript-eslint/no-var-requires
-//         .version(require('../../package.json').version);
+    program
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        .version(require('../../package.json').version);
 
-//     const fileExtensions = ReqSpecLanguageMetaData.fileExtensions.join(', ');
-//     program
-//         .command('generate')
-//         .argument('<file>', `source file (possible file extensions: ${fileExtensions})`)
-//         .option('-d, --destination <dir>', 'destination directory of generating')
-//         .description('generates JavaScript code that prints "Hello, {name}!" for each greeting in a source file')
-//         .action(generateAction);
+    const fileExtensions = ReqSpecLanguageMetaData.fileExtensions.join(', ');
+    program
+        .command('generate')
+        .argument('<file>', `source file (possible file extensions: ${fileExtensions})`)
+        .argument('[type]', 'Type of ReqSpec objects to export', 'Goal')
+        .option('-d, --destination <dir>', 'destination directory of generating')
+        .description('generates JavaScript code that prints "Hello, {name}!" for each greeting in a source file')
+        .action(generateAction);
 
-//     program.parse(process.argv);
-// }
+    program.parse(process.argv);
+}
