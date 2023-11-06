37211cf57612b063f0160448c3ba0fe57c7cd9b5
Andrew LaMarche <andrew.lamarche@uconn.edu>
2023-11-06 10:13:14 -0500

    Added 'conflicting' prop to Requirement

--- src/language/req-spec-validator.ts
+++ src/language/req-spec-validator.ts
@@ -1,6 +1,6 @@
 import { AstNode, ValidationAcceptor, ValidationChecks, Reference, Properties } from 'langium';
 // import { Category, CompFunction, Developer, Goal, GoalDocument, GoalSet, ReqSpecAstType, Requirement, RequirementDocument, RequirementSet } from './generated/ast';
-import { ReqSpecAstType, Requirement } from './generated/ast';
+import { Goal, ReqSpecAstType, Requirement } from './generated/ast';
 
 import type { ReqSpecServices } from './req-spec-module';
 
@@ -18,7 +18,8 @@ export function registerValidationChecks(services: ReqSpecServices) {
 
     const checks: ValidationChecks<ReqSpecAstType> = {
         AstNode:  validator.checkStartsWithCapital,
-        Requirement: validator.checkSelfReference<Requirement>
+        Requirement: validator.checkSelfReference<Requirement>,
+        Goal: validator.checkSelfReference<Goal>
     };
 
     registry.register(checks, validator);
@@ -36,10 +37,12 @@ function _checkSelfReference<T extends AstNode>(self:T, refs: Array<Reference<T>
 }
 
 interface ReqGoalRefs extends AstNode {
+    name: string
     decomposes?: Array<Reference>
     evolves?: Array<Reference>
     inherits?: Array<Reference>
     refines?: Array<Reference>
+    conflicting?: Array<Reference>
 }
 
 /**
@@ -60,19 +63,37 @@ export class ReqSpecValidator {
         }
     }
 
-    checkSelfReference<T extends ReqGoalRefs, AstNode>(item: T, accept: ValidationAcceptor): void { //<T extends AstNode & {name: string}>
+    /**
+     * Ensures that Requirement and Goal type references 
+     * 
+     * TODO - This really should defined from a generalized function. Lots of repetition. Just struggling with the TS typing
+     * 
+     * @param item Requirement or Goal type
+     * @param accept 
+     */
+    checkSelfReference<T extends ReqGoalRefs>(item: T, accept: ValidationAcceptor): void { //<T extends AstNode & {name: string}>
         
-        const keys: keyOmit<keyof ReqGoalRefs, keyof AstNode>[] = ["decomposes", "evolves", "inherits", "refines"]
+        if ( item.decomposes && _checkSelfReference(item, item.decomposes) ) // TODO - type casting shouldn't be needed
+            accept('error', `${item.$type} '${item.name}' cannot decompose itself`, 
+            { node: item, property: 'decomposes' as Properties<T>, code: ReqSpecIssueCodes.SelfReference } )
+        
+        if ( item.evolves && _checkSelfReference(item, item.evolves) ) // TODO - type casting shouldn't be needed
+            accept('error', `${item.$type} '${item.name}' cannot evolve itself`, 
+            { node: item, property: 'evolves' as Properties<T>, code: ReqSpecIssueCodes.SelfReference } )
 
-            for (const k of keys) {
-                if ( item[k as keyof ReqGoalRefs] && _checkSelfReference(item, item[k as keyof T] as Array<Reference<T>>) ) // TODO - type casting shouldn't be needed
-                    accept('error', `${item.$type} cannot decompose itself`, 
-                    { node: item, property: 'decomposes' as Properties<T>, code: ReqSpecIssueCodes.SelfReference } )
-                }
+        if ( item.refines && _checkSelfReference(item, item.refines) ) // TODO - type casting shouldn't be needed
+            accept('error', `${item.$type} '${item.name}' cannot refine itself`, 
+            { node: item, property: 'inherits' as Properties<T>, code: ReqSpecIssueCodes.SelfReference } )
+
+        if ( item.inherits && _checkSelfReference(item, item.inherits) ) // TODO - type casting shouldn't be needed
+            accept('error', `${item.$type} '${item.name}' cannot inherit from itself`, 
+            { node: item, property: 'refines' as Properties<T>, code: ReqSpecIssueCodes.SelfReference } )
+    
+        if ( item.conflicting && _checkSelfReference(item, item.conflicting) ) // TODO - type casting shouldn't be needed
+            accept('error', `${item.$type} '${item.name}' cannot inherit from itself`, 
+            { node: item, property: 'conflicting' as Properties<T>, code: ReqSpecIssueCodes.SelfReference } )
     }
+        
 
-    // checkGoalSelfReference(goal: Goal, accept: ValidationAcceptor): void {
-
-    // }
 
 }
