48bfb68521bcdf769b45adf48ac76f173fe87c8c
Andrew LaMarche <andrew.lamarche@uconn.edu>
2023-11-11 14:39:48 -0500

    converted generator to use expandToString

--- src/cli/generator.ts
+++ src/cli/generator.ts
@@ -1,5 +1,5 @@
 import fs from 'fs';
-import { CompositeGeneratorNode, Reference, toString } from 'langium';
+import { CompositeGeneratorNode, Reference, expandToString, toString } from 'langium';
 import path from 'path';
 import { Goal, Model, Requirement } from '../language/generated/ast';
 import { extractDestinationAndName } from './cli-util';
@@ -22,26 +22,29 @@ export function generateSphinxNeedsObjects(model: Model, type: NeedsObject = "Re
             switch (item.$type) { // if a given model item is the type we are seeking
                 case "Goal": 
                     let goal = item as Goal
-                    fileNode.append(`.. need:: ${goal.title ?? 'Placeholder Title'}\n` +
-                                        `\t:id: ${goal.name}\n` +
-                                        `${arrayRefStringer(goal.categories, 'tags')}` +
-                                        `${arrayRefStringer(goal.references, 'referencedGoals')}` +
-                                        `${arrayRefStringer(goal.evolves, 'evolves')}` +
-                                        `${arrayRefStringer(goal.refines, 'refines')}` +
-                                        `${arrayRefStringer(goal.conflicting, 'conflictsWith')}\n`
+                    fileNode.append(expandToString`
+                        .. need:: ${goal.title ?? 'Placeholder Title'}
+                            :id: ${goal.name}
+                            ${arrayRefStringer(goal.categories, 'tags')}
+                            ${arrayRefStringer(goal.references, 'referencedGoals')}
+                            ${arrayRefStringer(goal.evolves, 'evolves')}
+                            ${arrayRefStringer(goal.refines, 'refines')}
+                            ${arrayRefStringer(goal.conflicting, 'conflictsWith')}`
                     )
                     break
                 case "Requirement":
                     let req = item as Requirement
-                    fileNode.append(`.. req:: ${req.title ?? 'Placeholder Title'}\n` +
-                                        `\t:id: ${req.name}\n` +
-                                        `${arrayRefStringer(req.categories, 'tags')}` +
-                                        `${arrayRefStringer(req.references, 'referencedGoals')}` +
-                                        `${arrayRefStringer(req.evolves, 'evolves')}` +
-                                        `${arrayRefStringer(req.refines, 'refines')}` +
-                                        `${arrayRefStringer(req.inherits, 'inherits')}` +
-                                        `${arrayRefStringer(req.mitigatedHazards, 'mitigates')}` +
-                                        `${arrayRefStringer(req.decomposes, 'decomposes')}\n`)
+                    fileNode.append(expandToString`
+                        .. req:: ${req.title ?? 'Placeholder Title'}
+                            :id: ${req.name}
+                            ${arrayRefStringer(req.categories, 'tags')}
+                            ${arrayRefStringer(req.references, 'referencedGoals')}
+                            ${arrayRefStringer(req.evolves, 'evolves')}
+                            ${arrayRefStringer(req.refines, 'refines')}
+                            ${arrayRefStringer(req.inherits, 'inherits')}
+                            ${arrayRefStringer(req.mitigatedHazards, 'mitigates')}
+                            ${arrayRefStringer(req.decomposes, 'decomposes')}`
+                    )
                     break
             }
         }
